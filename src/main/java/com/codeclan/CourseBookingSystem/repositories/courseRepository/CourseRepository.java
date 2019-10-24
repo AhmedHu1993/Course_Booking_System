7dd6c118bc3db4133fa43f73ba2f1987c6cc4793
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-24 11:59:52 +0100

    test find course by rating

--- src/main/java/com/codeclan/CourseBookingSystem/repositories/courseRepository/CourseRepository.java
+++ src/main/java/com/codeclan/CourseBookingSystem/repositories/courseRepository/CourseRepository.java
@@ -3,5 +3,9 @@ package com.codeclan.CourseBookingSystem.repositories.courseRepository;
 import com.codeclan.CourseBookingSystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCoursesByStarRating(int starRating);
 }
--- src/test/java/com/codeclan/CourseBookingSystem/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.codeclan.CourseBookingSystem;
 
+import com.codeclan.CourseBookingSystem.models.Course;
+import com.codeclan.CourseBookingSystem.repositories.courseRepository.CourseRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class CourseBookingSystemApplicationTests {
+
+	@Autowired
+	CourseRepository courseRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void canFindCoursesByStarRating(){
+		List<Course> foundCourses = courseRepository.findCoursesByStarRating(4);
+		assertEquals(4 , foundCourses.get(0).getStarRating());
+	}
 }
